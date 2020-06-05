3f28974789d9492a3b3122191a1dc1e5a5b0db8a
Fadoua Ait Amrou <faitamrou.90@gmail.com>
2020-06-05 00:31:24 -0400

    WORD SEARCH GAME
    * Created a “Find a word game” that displays a grid of alphabets that’s 5x5 in size, with 4 hidden words
    * Then asked users what words they are able to find.
    * If they put in an incorrect word, ask them to try again. If they put in the right word, tell them how many words are left.
    If they are able to find all the words, give them some Swag points

---
+++ WordSearch/WordSearch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordSearch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> strList = new List<string>();
+            string[] words = new string[] { "BLAME", "VOICE", "LEAD", "NICE" };
+            strList.AddRange(words);
+
+
+            Console.WriteLine("C M L A M");
+            Console.WriteLine("A B O E D");
+            Console.WriteLine("I T N D G");
+            Console.WriteLine("C Y V I A");
+            Console.WriteLine("L E C O L");
+            Console.WriteLine("\n Were you able to find some words? \n");
+            
+
+            while(strList.Count != 0)
+            {
+                string name = Console.ReadLine();
+                if (name != "BLAME" && name != "VOICE" && name != "LEAD" && name != "NICE")
+                {
+                    Console.WriteLine("Try again");
+                    
+                }
+                else if (strList.Contains(name))
+                {
+                    strList.Remove(name);
+                    if (strList.Count() == 0)
+                    {
+                        Console.WriteLine("CONGRATULATIONS!!!!!");
+                    }
+                    else if (strList.Count() != 0)
+                    {
+                        Console.WriteLine("You have " + strList.Count() + " more");
+                        Console.WriteLine("you wanna finish the game");
+
+                    }
+
+
+                }
+            }
+        }
+    }
+}
